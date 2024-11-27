52b213bdfb88f949a478bf1860b5d5af3272235d
Kristifor Milchev <km@airzen.io>
2024-11-27 14:23:29 +0200

    Adding the users table.

---
+++ db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS Users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    certificate TEXT NOT NULL,
+    name TEXT NOT NULL,
+    created_at TEXT DEFAULT (datetime('now', 'localtime')),
+);
+
+
+
