df5670ee66c010ae1afad1582a10735c3d0c3939
Kristifor Milchev <km@airzen.io>
2024-11-28 18:02:47 +0200

    Implementing add-user.

--- commands/add_user.go
+++ commands/add_user.go
@@ -1,39 +1,125 @@
 package commands
 
 import (
+	"crypto/x509"
+	"encoding/hex"
 	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 
+	"client/interfaces"
 	"client/repositories"
 )
 
 type AddUserCommand struct {
-	UsersRepository repositories.UsersRepository
+	PasswordManager    interfaces.PasswordManager
+	UsersRepository    repositories.UsersRepository
+	UserKeysRepository repositories.KeysRepository
+	UserCertificates   repositories.Certificate
+	CertificateService interfaces.CertificateService
+	KeysService        interfaces.KeyService
 }
 
 func (u *AddUserCommand) Executable() *cobra.Command {
 	return &cobra.Command{
-		Use:   "add-user [certificate path] [name]",
+		Use:   "add-user [name]",
 		Short: "Add a new user to the contact list",
-		Args:  cobra.ExactArgs(1),
+		Args:  cobra.ExactArgs(0),
 		Run: func(cmd *cobra.Command, args []string) {
-			certificate := args[0]
-			name := args[1]
-
-			u.Execute(&certificate, &name)
+			name := args[0]
+			u.Execute(&name)
 		},
 	}
 }
 
-func (u *AddUserCommand) Execute(certificatePath *string, name *string) {
-	file, err := os.ReadFile(*certificatePath)
+func (u *AddUserCommand) Execute(name *string) {
+	fmt.Println("Please enter your account password")
+	var password string
+	fmt.Scan(&password)
+
+	created, err := u.UsersRepository.AddUser(name)
+
 	if err != nil {
-		fmt.Println("File doesn't exist!")
-		os.Exit(1)
+		fmt.Println("Failed to create user, can't proceed")
 		return
 	}
-	u.UsersRepository.AddUser(&file, name)
+
+	pub, priv, err := u.KeysService.IssueED25519Key()
+	if err != nil {
+		fmt.Println("failed to generate ED25519 key aborting!")
+		return
+	}
+
+	encryptedHexBytes, err := u.PasswordManager.Encrypt(*pub, []byte(password))
+	if err != nil {
+		fmt.Println("Failed to encrypt public key hex bytes")
+		return
+	}
+
+	encryptedHexBytesHex := hex.EncodeToString(*encryptedHexBytes)
+	ed25515PublicType := 2
+	u.UserKeysRepository.AddKey(&created, &ed25515PublicType, &encryptedHexBytesHex)
+
+	encryptPrivKey, err := u.PasswordManager.Encrypt(*priv, []byte(password))
+	if err != nil {
+		fmt.Println("Failed to encrypt private key hex bytes")
+		return
+	}
+	encryptPrivKeyHex := hex.EncodeToString(*encryptPrivKey)
+	ed25515PrivateType := 3
+	u.UserKeysRepository.AddKey(&created, &ed25515PrivateType, &encryptPrivKeyHex)
+
+	cert, err := u.CertificateService.IssueIdentityCertificate(pub, priv)
+
+	if err != nil {
+		fmt.Println("Failed to generate identity certificate with the given keys, aborting!")
+		return
+	}
+
+	identitiyCertificateType := 1
+	addUserCertificate := u.UserCertificates.AddCertificate(&created, &identitiyCertificateType, cert)
+
+	if !addUserCertificate {
+		fmt.Println("Failed to save user certificate aborting!")
+		return
+	}
+
+	pk, err := u.KeysService.IssueRSAKey(4096)
+
+	if err != nil {
+		fmt.Println("failed to generate encryption key aborting!")
+		return
+	}
+
+	privBytes := x509.MarshalPKCS1PrivateKey(pk)
+
+	encryptionPk, err := u.PasswordManager.Encrypt(privBytes, []byte(password))
+	if err != nil {
+		fmt.Println("Failed to save the encryption private key aborting!")
+		return
+	}
+	encryptionPkHex := hex.EncodeToString(*encryptionPk)
+	encryptionKeyType := 1
+	keySaved := u.UserKeysRepository.AddKey(&created, &encryptionKeyType, &encryptionPkHex)
+
+	if !keySaved {
+		fmt.Println("Failed to save key to the database, aborting!")
+		return
+	}
+
+	encryptionCertificate, err := u.CertificateService.IssueEncryptionCertificate(pk)
+
+	if err != nil {
+		fmt.Println("Failed to create encryption certificate aborting!")
+		return
+	}
+	encryptionCertificateType := 2
+	certificateCreated := u.UserCertificates.AddCertificate(&created, &encryptionCertificateType, encryptionCertificate)
+
+	if !certificateCreated {
+		fmt.Println("Failed to save encryption certificate aborting!")
+		return
+	}
 	os.Exit(0)
 }
