266c8ac325ab0bb3a60f8f3ef54c540e1d7d1016
Kristifor Milchev <km@airzen.io>
2024-11-28 17:56:20 +0200

    Removing the certificate from the usrs table.

--- db.sql
+++ db.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS Users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    certificate TEXT NOT NULL,
     name TEXT NOT NULL,
     created_at TEXT DEFAULT (datetime('now', 'localtime'))
 );
