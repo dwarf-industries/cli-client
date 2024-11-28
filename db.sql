af8f067cd6870451f7d223ab20e8e83cad4df53c
Kristifor Milchev <km@airzen.io>
2024-11-28 14:02:57 +0200

    Resolving an issue with the database schema.

--- db.sql
+++ db.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS Users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     certificate TEXT NOT NULL,
     name TEXT NOT NULL,
-    created_at TEXT DEFAULT (datetime('now', 'localtime')),
+    created_at TEXT DEFAULT (datetime('now', 'localtime'))
 );
 
 
