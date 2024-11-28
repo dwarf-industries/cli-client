085bd4433ddcbab92280cbf9d399a726e2b1825f
Kristifor Milchev <km@airzen.io>
2024-11-28 16:17:25 +0200

    Adding the keys repository.

---
+++ repositories/keys.go
@@ -0,0 +1,73 @@
+package repositories
+
+import (
+	"fmt"
+
+	"client/interfaces"
+	"client/models"
+)
+
+type KeysRepository struct {
+	storage interfaces.Storage
+}
+
+func (k *KeysRepository) UserKeys(userId *int) (*[]models.UserKey, error) {
+	sql := `
+		SELECT k.id, u.id, k.key_data, k.created_at FROM User_Keys as k
+		JOIN Users as u on k.user_id = u.id
+		WHERE u.id = $1
+	`
+
+	query, err := k.storage.Query(&sql, &[]interface{}{
+		&userId,
+	})
+
+	if err != nil {
+		fmt.Println("Failed to fetch user keys")
+		return nil, err
+	}
+
+	var userKeys []models.UserKey
+	for query.Next() {
+		var key models.UserKey
+		err := query.Scan(&key.Id, &key.UserId, &key.KeyData, &key.CreatedAt)
+
+		if err != nil {
+			fmt.Println("Failed to bind data, to model key aborting")
+			return nil, err
+		}
+
+		userKeys = append(userKeys, key)
+	}
+
+	return &userKeys, nil
+}
+
+func (k *KeysRepository) AddKey(userId *int, keyType *int, data *string) bool {
+	sql := `
+		INSERT INTO User_Keys (key_data, user_id, key_type) VALUES ($1,$2,$3)
+	`
+
+	err := k.storage.Exec(&sql, &[]interface{}{
+		&userId, &keyType, &data,
+	})
+
+	return err == nil
+}
+
+func (k *KeysRepository) DeleteUserKey(id *int) bool {
+	sql := `
+		DELETE FROM User_keys
+		WHERE id = $1
+	`
+
+	err := k.storage.Exec(&sql, &[]interface{}{
+		&id,
+	})
+
+	return err == nil
+}
+
+func (k *KeysRepository) Init(storage interfaces.Storage) {
+	k.storage = storage
+}
