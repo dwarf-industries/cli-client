976c9d516d6ef5b2f7e83bb9507d3a78b5eeb005
Kristifor Milchev <km@airzen.io>
2024-11-28 14:51:51 +0200

    Updating the database schema.

--- db.sql
+++ db.sql
@@ -6,4 +6,19 @@ CREATE TABLE IF NOT EXISTS Users (
 );
 
 
+CREATE TABLE IF NOT EXISTS User_Keys (
+    id INTEGER PRIMARY KEY AUTOINCREMENT
+    key_data TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TEXT DEFAULT (datetime('now', 'localtime')),
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
+)
+
+CREATE TABLE IF NOT EXISTS User_Certificates (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    certificate_data TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TEXT DEFAULT (datetime('now', 'localtime')),
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
+)
 
