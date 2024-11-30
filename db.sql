dc791c38358913ed3e051f035bcd02b00c1c0c58
Kristifor Milchev <km@airzen.io>
2024-11-30 11:15:45 +0200

    Resolving databse issues adding the accounts table.

--- db.sql
+++ db.sql
@@ -4,16 +4,15 @@ CREATE TABLE IF NOT EXISTS Users (
     created_at TEXT DEFAULT (datetime('now', 'localtime'))
 );
 
-
 CREATE TABLE IF NOT EXISTS User_Keys (
-    id INTEGER PRIMARY KEY AUTOINCREMENT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     key_data TEXT NOT NULL,
     user_id INTEGER NOT NULL,
     key_type INTEGER NOT NULL,
     created_at TEXT DEFAULT (datetime('now', 'localtime')),
     FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE,
     FOREIGN KEY (key_type) REFERENCES Key_Types(id) ON DELETE CASCADE
-)
+);
 
 CREATE TABLE IF NOT EXISTS User_Certificates (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,15 +22,19 @@ CREATE TABLE IF NOT EXISTS User_Certificates (
     created_at TEXT DEFAULT (datetime('now', 'localtime')),
     FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE,
     FOREIGN KEY (certificate_type) REFERENCES Certificate_Types(id) ON DELETE CASCADE
-)
+);
 
 CREATE TABLE IF NOT EXISTS Key_Types (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL
-)
-
+);
 
 CREATE TABLE IF NOT EXISTS Certificate_Types (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL
-)
+);
+
+CREATE TABLE IF NOT EXISTS Accounts (
+    key TEXT,
+    password TEXT
+);
