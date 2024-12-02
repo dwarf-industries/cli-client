56c23c5aa1233e8b5e125d0c47f81900639ede17
Kristifor Milchev <km@airzen.io>
2024-12-02 18:36:08 +0200

    Implementing the business rules for the connect command.

--- commands/connect.go
+++ commands/connect.go
@@ -10,6 +10,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"client/interfaces"
+	"client/models"
 	"client/repositories"
 )
 
@@ -18,6 +19,8 @@ type ConnectCommand struct {
 	AuthenticationService interfaces.AuthenticationService
 	UsersRepository       repositories.UsersRepository
 	SocketService         interfaces.SocketConnection
+	NodeRepository        repositories.NodesRepository
+	RegisterService       interfaces.RegisterService
 }
 
 func (c *ConnectCommand) Executable() *cobra.Command {
@@ -53,8 +56,55 @@ func (c *ConnectCommand) Execute(userId *int) {
 		panic("User doesn't exist")
 	}
 
+	selectedNodes, err := c.NodeRepository.Selected()
+	if err != nil {
+		fmt.Println("There are no node preferences")
+		fmt.Println("Warning: Since there are no node preferences you're about to connect to the entire network for sharding")
+		fmt.Println("This can lead to expensive network transfers, are you sure you want  are you sure you want to proceed")
+		fmt.Println("Enter 'y' to accept or anything else to cancel the action")
+
+		var answer string
+		fmt.Scanf(answer)
+
+		if answer != "y" {
+			os.Exit(1)
+		}
+	}
+
+	nodes, err := c.RegisterService.Oracles()
+	if err != nil {
+		fmt.Println("Failed to get nodes, aborting!")
+		os.Exit(1)
+	}
+
+	var currentNodes []models.Node
+	for _, n := range *selectedNodes {
+		selected := c.selectedNode(n, &nodes)
+
+		if selected == nil {
+			continue
+		}
+
+		currentNodes = append(currentNodes, *selected)
+	}
+
+	//Well if a man wants the full power, we give it, we don't question or ask why.
+	if len(currentNodes) == 0 {
+		currentNodes = nodes
+	}
+
+	for _, n := range currentNodes {
+		go c.ConnectToNode(&n, &user)
+
+	}
+
+	os.Exit(0)
+}
+
+func (c *ConnectCommand) ConnectToNode(node *models.Node, user *models.User) {
+
 	url := ""
-	challenge, err := c.AuthenticationService.Authenticate(url, &user.Certificate)
+	challenge, err := c.AuthenticationService.Authenticate(url, &user.Identity)
 
 	if err != nil {
 		panic("Failed to produce challenge, can't establish link with the node")
@@ -77,7 +127,7 @@ func (c *ConnectCommand) Execute(userId *int) {
 
 	handshake := map[string]interface{}{
 		"action":          "verifyChallenge",
-		"certificate":     user.Certificate,
+		"certificate":     user.Identity,
 		"signedChallenge": signature,
 		"sessionToken":    token,
 	}
@@ -88,5 +138,14 @@ func (c *ConnectCommand) Execute(userId *int) {
 		panic("Failed to connect to node")
 	}
 
-	os.Exit(0)
+}
+
+func (c *ConnectCommand) selectedNode(name string, nodes *[]models.Node) *models.Node {
+	for _, n := range *nodes {
+		if n.Name == name {
+			return &n
+		}
+	}
+
+	return nil
 }
