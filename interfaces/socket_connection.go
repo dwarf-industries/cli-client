805a5489e3220a692f0d279d9103df479d5baae8
Kristifor Milchev <km@airzen.io>
2024-12-05 19:14:54 +0200

    Adding better connection handlig, recovery after timeout and a method to get inbound events from the socket.

--- interfaces/socket_connection.go
+++ interfaces/socket_connection.go
@@ -2,6 +2,7 @@ package interfaces
 
 type SocketConnection interface {
 	Connect(url *string, connectionData *map[string]interface{}) bool
+	SubscribeToChanges() *chan map[string]interface{}
 	SetToken(token *string)
 	Disconnect() bool
 	SendData(data *map[string]interface{}) *map[string]interface{}
--- services/socket_connection.go
+++ services/socket_connection.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -12,6 +13,9 @@ import (
 type SocketConnection struct {
 	connection *websocket.Conn
 	token      *string
+	messageCh  chan map[string]interface{}
+	url        *string
+	handshake  *map[string]interface{}
 }
 
 func (s *SocketConnection) Connect(url *string, handshake *map[string]interface{}) bool {
@@ -20,28 +24,84 @@ func (s *SocketConnection) Connect(url *string, handshake *map[string]interface{
 
 	conn, _, err := websocket.DefaultDialer.Dial(socketUrl, headers)
 	if err != nil {
-		log.Fatalf("Failed to connect to WebSocket: %v", err)
+		log.Printf("Failed to connect to WebSocket: %v", err)
+		return false
 	}
 
 	fmt.Println("Connected to WebSocket")
 
 	if err := conn.WriteJSON(handshake); err != nil {
-		log.Fatalf("Failed to send handshake: %v", err)
+		log.Printf("Failed to send handshake: %v", err)
+		return false
 	}
 
 	var authResponse map[string]interface{}
 	if err := conn.ReadJSON(&authResponse); err != nil {
-		log.Fatalf("Failed to read handshake response: %v", err)
+		log.Printf("Failed to read handshake response: %v", err)
+		return false
 	}
 	fmt.Printf("Handshake response: %v\n", authResponse)
 
 	if authResponse["State"] != "Authenticated" {
-		log.Fatalf("Authentication failed: %v", authResponse)
+		log.Printf("Authentication failed: %v", authResponse)
+		return false
 	}
 
 	s.connection = conn
+	s.messageCh = make(chan map[string]interface{})
+	s.url = url
+	s.handshake = handshake
+
+	go s.handlePing()
+	go s.listenForMessages()
 
 	return true
+}
+
+func (s *SocketConnection) reconnect() {
+	for {
+		log.Println("Attempting to reconnect...")
+		if s.Connect(s.url, s.handshake) {
+			log.Println("Reconnected successfully")
+			return
+		}
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func (s *SocketConnection) handlePing() {
+	for {
+		err := s.connection.WriteMessage(websocket.PingMessage, nil)
+		if err != nil {
+			log.Printf("Ping failed, WebSocket might be disconnected: %v", err)
+			close(s.messageCh)
+			go s.reconnect()
+			break
+		}
+		time.Sleep(30 * time.Second)
+	}
+}
+
+func (s *SocketConnection) listenForMessages() {
+	for {
+		var message map[string]interface{}
+		err := s.connection.ReadJSON(&message)
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseNormalClosure, websocket.CloseGoingAway) {
+				log.Printf("WebSocket closed unexpectedly: %v", err)
+			} else {
+				log.Printf("ReadJSON error: %v", err)
+			}
+			close(s.messageCh)
+			go s.reconnect()
+			return
+		}
+		s.messageCh <- message
+	}
+}
+
+func (s *SocketConnection) SubscribeToChanges() *chan map[string]interface{} {
+	return &s.messageCh
 }
 
 func (s *SocketConnection) SetToken(token *string) {
